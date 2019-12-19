0b5df5856d6bf9604777b5afdcecea80a605560b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-12-19 16:14:13 +0000

    Correcting the webpack file with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -13,51 +13,51 @@ module.exports = {
         use: [
           {
             // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
             // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
           {
             // Loader for webpack to process CSS with PostCSS
             loader: 'postcss-loader',
             options: {
-              plugins: function () {
+              plugins() {
                 return [
-                  require('autoprefixer')
+                  require('autoprefixer'),
                 ];
-              }
-            }
+              },
+            },
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
+            loader: 'sass-loader',
+          },
+        ],
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
         ],
       },
-       {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
-           'file-loader',
-         ],
-       },
-       {
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
         test: /\.(html)$/,
         use: {
           loader: 'html-loader',
           options: {
-            attrs: [':data-src']
-          }
-        }
-      }
-    ]
-  }
+            attrs: [':data-src'],
+          },
+        },
+      },
+    ],
+  },
 };
